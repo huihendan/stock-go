c373167ffad4dca7cd60d834940a70b29008992c
beven <bevenmiao@163.com>
2025-07-13 20:57:59 +0800

    [功能修改] 补充wechat单元测试

---
+++ exec/analyseDataEveryDay.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+}
+
+func updateStockData() {
+
+	//python3 D:/stock/stockServer/stockData/stockData.py
+
+}
---
+++ utils/wechat_test.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"testing"
+)
+
+func TestSendWeChatMessage(t *testing.T) {
+	SendWeChatMessage("test")
+}
